5726243ec2bd84ec5a319d7209e313b159196a33
Vlad Podvoiskyi <vladyslav.podvoiskyi@chisw.com>
2018-01-19 10:31:52 +0200

    created homeTabBarButton

---
+++ Eredivisie/Eredivisie/Controller/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  Eredivisie
+//
+//  Created by user on 1/19/18.
+//  Copyright © 2018 vlad. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UITabBarController{
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("wtf")
+    }
+
+}
