e5d4145055e9c22b19b3902c20f5141d7a06f8f8
Vlad Podvoiskyi <vladyslav.podvoiskyi@chisw.com>
2018-02-02 18:33:04 +0200

    create constraints for MatchDetailsCell

--- Eredivisie/Eredivisie/Controller/MatchDetailsViewController.swift
+++ Eredivisie/Eredivisie/Controller/MatchDetailsViewController.swift
@@ -15,7 +15,7 @@ class MatchDetailsViewController: UIViewController, UITableViewDelegate, UITable
     var matchDetailsHomeImageView: UIImageView = {
         let homeTeamImageView = UIImageView()
         homeTeamImageView.translatesAutoresizingMaskIntoConstraints = false
-        homeTeamImageView.contentMode = .scaleAspectFill
+        homeTeamImageView.contentMode = .scaleToFill
         homeTeamImageView.backgroundColor = .red
 //        homeTeamImageView.image = UIImage(named: "matchDetailsTeamHomeLabel")
         
@@ -26,7 +26,7 @@ class MatchDetailsViewController: UIViewController, UITableViewDelegate, UITable
         let awayTeamImageView = UIImageView()
         awayTeamImageView.translatesAutoresizingMaskIntoConstraints = false
         awayTeamImageView.backgroundColor = .yellow
-        awayTeamImageView.contentMode = .scaleAspectFill
+        awayTeamImageView.contentMode = .scaleToFill
 //        awayTeamImageView.image = UIImage(named: "matchDetailsTeamAwayLabel")
         
         return awayTeamImageView
@@ -35,7 +35,7 @@ class MatchDetailsViewController: UIViewController, UITableViewDelegate, UITable
     var matchDetailsTeamHomeLabel: UILabel = {
         let teamHomeLabel = UILabel()
         teamHomeLabel.translatesAutoresizingMaskIntoConstraints = false
-        teamHomeLabel.backgroundColor = .gray
+        teamHomeLabel.font = UIFont.boldSystemFont(ofSize: 20)
         teamHomeLabel.contentMode = .scaleAspectFill
         teamHomeLabel.textAlignment = .center
         teamHomeLabel.text = "Roda JC Kerkrade"
@@ -46,11 +46,10 @@ class MatchDetailsViewController: UIViewController, UITableViewDelegate, UITable
     var matchDetailsTeamAwayLabel: UILabel = {
         let teamAwayLabel = UILabel()
         teamAwayLabel.translatesAutoresizingMaskIntoConstraints = false
-        teamAwayLabel.backgroundColor = .green
+        teamAwayLabel.font = UIFont.boldSystemFont(ofSize: 20)
         teamAwayLabel.contentMode = .scaleAspectFill
         teamAwayLabel.textAlignment = .center
         teamAwayLabel.text = "Roda JC Kerkrade"
-        teamAwayLabel.textColor = UIColor.purple
         
         return teamAwayLabel
     }()
@@ -60,6 +59,7 @@ class MatchDetailsViewController: UIViewController, UITableViewDelegate, UITable
         let goalHomeLabel = UILabel()
         goalHomeLabel.translatesAutoresizingMaskIntoConstraints = false
         goalHomeLabel.textAlignment = .center
+        goalHomeLabel.font = UIFont.boldSystemFont(ofSize: 30)
         goalHomeLabel.text = "88"
         
         return goalHomeLabel
@@ -70,6 +70,7 @@ class MatchDetailsViewController: UIViewController, UITableViewDelegate, UITable
         let goalAwayLabel = UILabel()
         goalAwayLabel.translatesAutoresizingMaskIntoConstraints = false
         goalAwayLabel.textAlignment = .center
+        goalAwayLabel.font = UIFont.boldSystemFont(ofSize: 30)
         goalAwayLabel.text = "86"
         
         return goalAwayLabel
@@ -84,6 +85,7 @@ class MatchDetailsViewController: UIViewController, UITableViewDelegate, UITable
         self.createMatchDetailsTableView()
         self.matchDetailsTableView.reloadData()
         self.setup(with: statistics)
+        self.createBackgroundMatchDetailsView()
 
         // Do any additional setup after loading the view.
     }
@@ -102,16 +104,16 @@ class MatchDetailsViewController: UIViewController, UITableViewDelegate, UITable
     func makeConstrains() {
 //        create constraints for matchDetailsHomeImageView
         self.view.addSubview(matchDetailsHomeImageView)
-        matchDetailsHomeImageView.widthAnchor.constraint(equalToConstant: 130).isActive = true
-        matchDetailsHomeImageView.heightAnchor.constraint(equalToConstant: 130).isActive = true
+        matchDetailsHomeImageView.widthAnchor.constraint(equalToConstant: 140).isActive = true
+        matchDetailsHomeImageView.heightAnchor.constraint(equalToConstant: 140).isActive = true
         matchDetailsHomeImageView.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 105).isActive = true
         matchDetailsHomeImageView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 40).isActive = true
 
         
 //        create constraints for matchDetailsHomeImageView
         self.view.addSubview(matchDetailsAwayImageView)
-        matchDetailsAwayImageView.widthAnchor.constraint(equalToConstant: 130).isActive = true
-        matchDetailsAwayImageView.heightAnchor.constraint(equalToConstant: 130).isActive = true
+        matchDetailsAwayImageView.widthAnchor.constraint(equalToConstant: 140).isActive = true
+        matchDetailsAwayImageView.heightAnchor.constraint(equalToConstant: 140).isActive = true
         matchDetailsAwayImageView.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 105).isActive = true
         matchDetailsAwayImageView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -40).isActive = true
         
@@ -119,34 +121,34 @@ class MatchDetailsViewController: UIViewController, UITableViewDelegate, UITable
         
 //        create constraints for matchDetailsHomeGoalsLabel
         self.view.addSubview(matchDetailsHomeGoalsLabel)
-        matchDetailsHomeGoalsLabel.widthAnchor.constraint(equalToConstant: 50).isActive = true
-        matchDetailsHomeGoalsLabel.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        matchDetailsHomeGoalsLabel.widthAnchor.constraint(equalToConstant: 60).isActive = true
+        matchDetailsHomeGoalsLabel.heightAnchor.constraint(equalToConstant: 60).isActive = true
         matchDetailsHomeGoalsLabel.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 250).isActive = true
         matchDetailsHomeGoalsLabel.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 80).isActive = true
         
         
 //        create constraints for matchDetailsAwayGoalsLabel
         self.view.addSubview(matchDetailsAwayGoalsLabel)
-        matchDetailsAwayGoalsLabel.widthAnchor.constraint(equalToConstant: 50).isActive = true
-        matchDetailsAwayGoalsLabel.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        matchDetailsAwayGoalsLabel.widthAnchor.constraint(equalToConstant: 60).isActive = true
+        matchDetailsAwayGoalsLabel.heightAnchor.constraint(equalToConstant: 60).isActive = true
         matchDetailsAwayGoalsLabel.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 250).isActive = true
         matchDetailsAwayGoalsLabel.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -80).isActive = true
         
         
 //        create constraints for matchDetailsTeamHomeLabel
         self.view.addSubview(matchDetailsTeamHomeLabel)
-        matchDetailsTeamHomeLabel.widthAnchor.constraint(equalToConstant: 140).isActive = true
+        matchDetailsTeamHomeLabel.widthAnchor.constraint(equalToConstant: 170).isActive = true
         matchDetailsTeamHomeLabel.heightAnchor.constraint(equalToConstant: 30).isActive = true
         matchDetailsTeamHomeLabel.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 310).isActive = true
-        matchDetailsTeamHomeLabel.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 40).isActive = true
+        matchDetailsTeamHomeLabel.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 30).isActive = true
         
         
 //        create constraints for matchDetailsTeamAwayLabel
         self.view.addSubview(matchDetailsTeamAwayLabel)
-        matchDetailsTeamAwayLabel.widthAnchor.constraint(equalToConstant: 140).isActive = true
+        matchDetailsTeamAwayLabel.widthAnchor.constraint(equalToConstant: 170).isActive = true
         matchDetailsTeamAwayLabel.heightAnchor.constraint(equalToConstant: 30).isActive = true
         matchDetailsTeamAwayLabel.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 310).isActive = true
-        matchDetailsTeamAwayLabel.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -40).isActive = true
+        matchDetailsTeamAwayLabel.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -30).isActive = true
     
     
 //        create constraints for matchDetailsTableView
@@ -163,15 +165,14 @@ class MatchDetailsViewController: UIViewController, UITableViewDelegate, UITable
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = self.matchDetailsTableView.dequeueReusableCell(withIdentifier: "detailCell", for: indexPath) as! MatchDetailsCell
-        cell.textLabel?.text = "\(indexPath.section) cell \(indexPath.row)"
-        //cell.contentView.backgroundColor = .green
+        
         return cell
     }
     
     
     //MARK - UITableViewDelegate
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 30
+        return 40
     }
     
     private func setup(with statistics: Statistics) {
@@ -181,5 +182,16 @@ class MatchDetailsViewController: UIViewController, UITableViewDelegate, UITable
         matchDetailsTeamAwayLabel.text = statistics.awayTeamName
         matchDetailsHomeGoalsLabel.text = String(statistics.homeTeamScore)
         matchDetailsAwayGoalsLabel.text = String(statistics.awayTeamScore)
+    }
+    
+    func createBackgroundMatchDetailsView() {
+        let colorTop = UIColor.orange.cgColor
+        let colorMiddle = UIColor.white.cgColor
+        let colorMiddle1 = UIColor.white.cgColor
+        let colorBottom = UIColor.blue.cgColor
+        
+        let colorsArray = [colorTop, colorMiddle, colorMiddle1, colorBottom]
+        
+        self.view.setGradientBackgroundUniversal(colors: colorsArray, start: CGPoint(x: 0, y: 0), end: CGPoint(x: 1, y: 1))
     }
 }
--- Eredivisie/Eredivisie/View/MatchDetailsCell.swift
+++ Eredivisie/Eredivisie/View/MatchDetailsCell.swift
@@ -9,10 +9,78 @@
 import UIKit
 
 class MatchDetailsCell: UITableViewCell {
+    
+    
+    var minuteOfEvents: UILabel = {
+        let time = UILabel()
+        time.text = "16"
+        time.translatesAutoresizingMaskIntoConstraints = false
+        
+        return time
+    }()
+    
+    var playerHome: UILabel = {
+        let pH = UILabel()
+        pH.text = "Holland"
+        pH.translatesAutoresizingMaskIntoConstraints = false
+        
+        return pH
+    }()
+    
+    var imageEventHome: UIImageView = {
+        let image = UIImageView()
+        image.contentMode = .scaleToFill
+        image.translatesAutoresizingMaskIntoConstraints = false
+        
+        return image
+    }()
+    
+    var currentResult: UILabel = {
+        let result = UILabel()
+        result.text = "10:10"
+        result.translatesAutoresizingMaskIntoConstraints = false
+        
+        return result
+    }()
+    
+    var imageEventAway: UIImageView = {
+        let image = UIImageView()
+        image.contentMode = .scaleToFill
+        image.translatesAutoresizingMaskIntoConstraints = false
+        
+        return image
+    }()
+    
+    var playerAway: UILabel = {
+       let pA = UILabel()
+        pA.text = "Netherlands"
+        pA.translatesAutoresizingMaskIntoConstraints = false
+        
+        return pA
+    }()
+    
+    private func makeView() {
+        createConstraints()
+    }
+    
+    
+    func createConstraints() {
+//        create constraints for minuteOfEvents
+        self.addSubview(minuteOfEvents)
+        currentResult.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+        currentResult.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+        currentResult.heightAnchor.constraint(equalTo: self.heightAnchor, multiplier: 0.5).isActive = true
+        
+    }
+    
+    
+    
+    
+    
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        self.makeView()
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
